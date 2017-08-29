39743f1eeb6acc2b98bd7fa2eac6272bdf587a8d
Peter Varholak <peter.varholak@globallogic.com>
2017-08-29 13:05:50 +0200

    study card buttonify

--- src/components/study-card/study-card.js
+++ src/components/study-card/study-card.js
@@ -5,19 +5,22 @@ import { Icon } from '../image';
 import type { Study } from '../../store';
 import { formatDate } from '../../util';
 
+// TODO: Adding 'study-card-button' makes text centered, I like it but need to double check with designer
 const StudyCard = ({ id, name, date, pictureCount, previewImage }: Study) => (
   <div className="study-card">
-    <img
-      src={previewImage} // TODO: image processing/component
-      alt={name}
-      className="preview-image"
-    />
-    <span className="text-highlight bold">{name}</span>
-    <div className="study-card-text">
-      <span>{formatDate(date)}</span>
-      <Icon name="numberOfPicture" />
-      <span className="text-light">{pictureCount}</span>
-    </div>
+    <a href={`/viewer/${ id }`} className="study-card-button study-card-body">
+      <img
+        src={previewImage} // TODO: image processing/component
+        alt={name}
+        className="preview-image"
+      />
+      <span className="text-highlight bold">{name}</span>
+      <div className="study-card-text">
+        <span>{formatDate(date)}</span>
+        <Icon name="numberOfPicture" />
+        <span className="text-light">{pictureCount}</span>
+      </div>
+    </a>
     <div className="study-card-buttons">
       <Link to={`/analysis/${ id }`} className="text-highlight study-card-button">Analyis</Link>
       <a href={`/viewer/${ id }`} className="text-highlight study-card-button">
