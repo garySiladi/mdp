ea10d0ba098ca003ba77f689ff85a980e25a6c98
Peter Varholak <peter.varholak@globallogic.com>
2017-09-06 10:02:30 +0200

    The finalest of them all

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,10 +1,13 @@
 import 'react-table/react-table.css';
 import TableWrapper from './table-wrapper';
 import TableViewer from './table-viewer';
+import SearchBar from './search-bar';
 import './table-wrapper.css';
 import './table-viewer.css';
+import './search-bar.css';
 
 export {
   TableWrapper,
   TableViewer,
+  SearchBar,
 };
--- src/components/table/table-wrapper.js
+++ src/components/table/table-wrapper.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchPatients, receivedPatients, selectPatient } from '../../api';
-import { TableViewer } from './';
+import TableViewer from './table-viewer';
+import SearchBar from './search-bar';
 import type { Patient } from '../../store';
 
 type Props = {
@@ -17,16 +18,29 @@ type Props = {
 };
 
 class TableWrapper extends React.Component<> {
+  static filterResults(list = [], keyword) {
+    return list.filter(patient => patient.name.includes(keyword));
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchedValue: '',
+    };
+  }
+
   componentWillMount() {
     fetchPatients(this.props.actions.dispatchReceivedPatients);
   }
+
   props:Props
   render() {
     const { data, actions } = this.props;
     return (
       <div>
+        <SearchBar onChange={e => { this.setState({ searchedValue: e.target.value }); }} />
         <TableViewer
-          data={data.patientList}
+          data={TableWrapper.filterResults(data.patientList, this.state.searchedValue)}
           handleSelectPatient={actions.dispatchSelectedPatient}
         />
       </div>
